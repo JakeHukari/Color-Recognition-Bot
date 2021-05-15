978004903b76b0e4497956ff88c951688ac40dd7
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-14 23:15:29 -0600

    Update Bot.py

---
+++ Bot.py
@@ -0,0 +1,23 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_pyLEFTDOWN,0,0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+    
+while keyboard.is_pressed('q') == False:
+    
+    if pyautogui.pixel(-1426, 555)[0] == 0:
+        click(-1426, 555)
+    if pyautogui.pixel(-1426, 555)[0] == 0:
+        click(-1339, 555)
+    if pyautogui.pixel(-1426, 555)[0] == 0:
+        click(-1260, 555)
+    if pyautogui.pixel(-1426, 555)[0] == 0:
+        click(-1150, 555)
